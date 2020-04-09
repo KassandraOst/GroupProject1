248cddf3577441db1461daa049958b4c23c50c67
Anish <shar0502@algonquinlive.com>
2020-04-09 16:18:22 -0400

    test

--- app/src/main/java/com/example/groupproject1/MainActivity.java
+++ app/src/main/java/com/example/groupproject1/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//note
         //Initiates all the buttons to the appropriate views
         newsButton = findViewById(R.id.mGuardBtn);
         imgDayButton = findViewById(R.id.mImgDayBtn);
